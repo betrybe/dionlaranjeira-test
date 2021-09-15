2388f007b609a3c4058800e2b41c1d066ed7a2e8
DION RIBEIRO LARANJEIRA <dionribeiro.rr@gmail.com>
2021-09-15 17:18:12 -0300

    Criação da tela de login
    Tela de login com estilo e sem funcionalidades.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import '../styles/login.css';
+import logoTrybe from '../assets/logo-trybe.png';
+
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return(
+    <div id="page-login" >
+      <form id="container-form" >
+       <img id="logo-trybe"  src={logoTrybe} alt="Logo Trybe"  ></img>
+        
+            <input type="email" name="email" data-testid="email-input" ></input>
+            <input type="password" name="password" data-testid="password-input" ></input>
+            <button value="Entrar" id="btn-entrar" >Entrar</button>
+        
+        </form>
+    </div>);
   }
 }
-
 export default Login;
